01b4c95986fda962357e6c4f43122664b9860f2f
David Adams <david.adams.16.12@gmail.com>
2023-11-04 20:56:44 +1100

    Remove old fixed games

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,21 +1,11 @@
 import { ReactNode, useMemo } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import MenuPage from './Menu';
-import GamePage from './GamePage';
-import { GameData, games } from '../games';
-import { Noun, Verb } from '../../types';
 import GameCustomisation from './GameCustomisation';
 import CustomGamePage from './CustomGamePage';
 
 export type PageId = (
   | 'menu'
-  | 'customise'
   | 'practice'
-  | 'nouns'
-  | 'present-verbs'
-  | 'indicative'
-  | 'imperative'
-  | 'infinitive'
 );
 
 export interface Page {
@@ -25,17 +15,11 @@ export interface Page {
   page: ReactNode,
 }
 
-export const functionalPages: Page[] = [
+export const pages: Page[] = [
   {
     id: 'menu',
     path: '/',
     name: 'Menu',
-    page: <MenuPage />,
-  },
-  {
-    id: 'customise',
-    path: '/customise',
-    name: 'Customise Study',
     page: <GameCustomisation />,
   },
   {
@@ -46,44 +30,9 @@ export const functionalPages: Page[] = [
   }
 ];
 
-export const gamePages: Page[] = [
-  {
-    id: 'nouns',
-    path: '/nouns/',
-    name: 'Nouns',
-    page: <GamePage game={games.firstAndSecondNouns as GameData<Noun.Data>} />,
-  },
-  {
-    id: 'present-verbs',
-    path: '/present-verbs/',
-    name: 'Present Active Indicative Verbs',
-    page: <GamePage game={games.verbs as GameData<Verb.Data>} />,
-  },
-  {
-    id: 'indicative',
-    path: '/indicative/',
-    name: 'Indicative Verbs',
-    page: <GamePage game={games.indicative as GameData<Verb.Data>} />,
-  },
-  {
-    id: 'imperative',
-    path: '/imperative/',
-    name: 'Imperative Verbs',
-    page: <GamePage game={games.imperative as GameData<Verb.Data>} />,
-  },
-  {
-    id: 'infinitive',
-    path: '/infinitive/',
-    name: 'Infinitive Verbs',
-    page: <GamePage game={games.infinitive as GameData<Verb.Data>} />,
-  },
-];
-
-const allPages = [...functionalPages, ...gamePages].reverse();
-
 function PageView() {
   const pageRoutes = useMemo(
-    () => allPages.map(page => (
+    () => pages.map(page => (
       <Route
         key={page.id}
         path={page.path}
@@ -104,7 +53,7 @@ function PageView() {
 export default PageView;
 
 export function getPage(page: PageId) {
-  const result = allPages.find(p => p.id === page);
+  const result = pages.find(p => p.id === page);
   if (result === undefined) {
     throw new Error(`Unknown page id ${page}`);
   }
